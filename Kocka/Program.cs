8dcfdbacc05965bd3bfe8e21982a01cf3b3b2f64
Gergő György Tóth <2020092@dszcberegszaszi.hu>
2023-11-15 11:26:03 +0100

    Add project files.

---
+++ Kocka/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kocka
+{
+    public class NegyszogClass
+    {
+        private int a=3, kerulet, terulet;
+
+        public void SetTerulet()
+        {
+            this.terulet = a * a;
+        }
+        public void SetKerulet()
+        {
+            this.kerulet = 4 * a;
+        }
+
+        public int GetTerulet()
+        {
+            return this.terulet;
+        }
+        public int GetKerulet()
+        {
+            return this.kerulet;
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            NegyszogClass negyszog = new NegyszogClass();
+            
+            negyszog.SetKerulet();
+            negyszog.SetTerulet();
+            Console.WriteLine("A Négyszög kerulete: {0} és területe: {0}");
+                negyszog.GetKerulet();
+                negyszog.GetTerulet();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KockaUnitTeszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KockaUnitTeszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KockaUnitTeszt")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7266c1f4-aed6-49c7-99e1-16962f35654f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
